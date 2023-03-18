06942c427bde15df3f79ba5e65db7ade1362f94e
Shahiryar Arif <shahiryararif@gmail.com>
2023-03-19 02:51:51 +0500

    Added the token into cookies

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -78,6 +78,10 @@ router.post('/signin', async (req, res) => {
       const isMatch = await bcrypt.compare(password, userLogin.password);
 
       const token = await userLogin.generateAuthToken();
+      res.cookie('jwtoken', token, {
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly: true,
+      });
       if (!isMatch) {
         res.status(400).json({ error: 'Invalid Credentials' });
       } else {
